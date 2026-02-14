3487d4a3922de17fbe81e81793abfeb52902e39c
mimma <mbjelanovic@gmail.com>
2026-02-14 13:51:57 +0100

    Remove Postman collection and add Flask-Migrate integration

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from config import Config
 from api.models import db
@@ -11,10 +12,13 @@ app.config.from_object(Config)
 
 # Initialize extensions
 db.init_app(app)
+migrate = Migrate(app, db)
 JWTManager(app)
 
 # Create upload folder if it doesn't exist
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 from api import models
-from api.ro
+from api.routes import auth
+from api.routes import video
+from api.routes import user
